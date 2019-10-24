3ce7e5966e9d9f20fab478919502e57fea8197e0
garryjable <garrettjamesjennings@gmail.com>
2019-10-23 22:41:13 -0600

    pushing up everything I have

--- shapes.py
+++ shapes.py
@@ -5,13 +5,13 @@ from tkinter import PhotoImage
 class Shape(ABC):
     xCoord = 0
     yCoord = 0
-    item_id = ''
+    item_id = ""
     measurements = {}
 
     def __init__(self, *args, **kwargs):
-        self.xCoord = kwargs['xCoord']
-        self.yCoord = kwargs['yCoord']
-        self.measurements = kwargs['measurements']
+        self.xCoord = kwargs["xCoord"]
+        self.yCoord = kwargs["yCoord"]
+        self.measurements = kwargs["measurements"]
 
     def get_coords(self):
         return (self.xCoord, self.yCoord)
@@ -21,14 +21,14 @@ class Shape(ABC):
         return
 
     def move(self, *args, **kwargs):
-        self.xCoord = kwargs['newXCoord'] 
-        self.yCoord = kwargs['newYCoord'] 
+        self.xCoord = kwargs["newXCoord"]
+        self.yCoord = kwargs["newYCoord"]
         print("moving the shape")
 
     def scale(self, *args, **kwargs):
         new_measurements = self.measurements
         for key, val in self.measurements.items():
-            new_measurements[key] = val * kwargs['factor']
+            new_measurements[key] = val * kwargs["factor"]
         self.measurements = new_measurements
 
     @abstractmethod
@@ -36,11 +36,11 @@ class Shape(ABC):
         drawables = []
         for component in self.components:
             drawables.append(component.get_drawable())
-        return drawables 
+        return drawables
 
 
 class CompositeShape(Shape):
-    components =[]
+    components = []
     # item id is a tinkter tag
 
     def compute_area(self, *args, **kwargs):
@@ -61,43 +61,39 @@ class CompositeShape(Shape):
         drawables = []
         for component in self.components:
             drawables.append(component.get_drawable())
-        return drawables 
+        return drawables
 
 
-class Rectangle(Shape): # tkinter polygon
-
+class Rectangle(Shape):  # tkinter polygon
     def compute_area(self, *args, **kwargs):
-        return self.measurements['width'] * self.measurements['length']
+        return self.measurements["width"] * self.measurements["length"]
 
     def get_drawable(self, *args, **kwargs):
         print("return tkinter obj")
 
 
-class Triangle(Shape): # tkinter polygon
-
+class Triangle(Shape):  # tkinter polygon
     def compute_area(self, *args, **kwargs):
-        return (4.33) * ( self.measurements['side'] ** 2 )
+        return (4.33) * (self.measurements["side"] ** 2)
 
     def get_drawable(self, *args, **kwargs):
         print("return tkinter obj")
 
 
-class Circle(Shape): # tkinter oval
-
-
+class Circle(Shape):  # tkinter oval
     def compute_area(self, *args, **kwargs):
-        return 3.14 * self.measurements['radius'] ** 2
+        return 3.14 * self.measurements["radius"] ** 2
 
     def get_drawable(self, *args, **kwargs):
         print("return tkinter obj")
 
 
 class Image(Rectangle):
-    filename = ''
+    filename = ""
 
     def __init__(self, *args, **kwargs):
         super(Image, self).__init__(*args, **kwargs)
-        self.filename = kwargs['filename']
+        self.filename = kwargs["filename"]
 
 
 class Line(Shape):
@@ -106,27 +102,25 @@ class Line(Shape):
 
     def __init__(self, *args, **kwargs):
         super(Line, self).__init__(*args, **kwargs)
-        self.xCoord = kwargs['xCoord2']
-        self.yCoord = kwargs['yCoord2']
+        self.xCoord = kwargs["xCoord2"]
+        self.yCoord = kwargs["yCoord2"]
 
     def compute_area(self, *args, **kwargs):
         return 0
 
     def move(self, *args, **kwargs):
         super(Line, self).move(*args, **kwargs)
-        self.xCoord2 = kwargs['newXCoord2']
-        self.yCoord2 = kwargs['newYCoord2']
+        self.xCoord2 = kwargs["newXCoord2"]
+        self.yCoord2 = kwargs["newYCoord2"]
 
     def get_coords(self):
         return [(self.xCoord, self.yCoord), (self.xCoord2, self.yCoord2)]
-
 
     def get_drawable(self):
         print("return tkinter obj")
 
 
-class Point(Shape): # tkinter oval with small radius
-
+class Point(Shape):  # tkinter oval with small radius
     def compute_area(self, *args, **kwargs):
         return 0
 
--- unit_tests.py
+++ unit_tests.py
@@ -1,29 +1,40 @@
 import unittest
-from shapeClasses import Shape, CompositeShape, Rectangle, Circle, Line, Point, Image, Triangle
+from shapes import (
+    Shape,
+    CompositeShape,
+    Rectangle,
+    Circle,
+    Line,
+    Point,
+    Image,
+    Triangle,
+)
 
 
 from tkinter import PhotoImage
 
-class ShapeTests():
 
-
+class ShapeTests:
     def test_compute_area(self):
         area = self.shape.compute_area()
         self.assertEqual(area, self.area)
 
     def scale_setUp(self):
         self.scale_kwargs = {}
-        self.scale_kwargs['factor'] = 2
+        self.scale_kwargs["factor"] = 2
         self.before = self.shape.compute_area()
         self.shape.scale(**self.scale_kwargs)
         self.after = self.shape.compute_area()
 
     def test_move(self):
         move_kwargs = {}
-        move_kwargs['newXCoord'] = 2
-        move_kwargs['newYCoord'] = 2
+        move_kwargs["newXCoord"] = 2
+        move_kwargs["newYCoord"] = 2
         self.shape.move(**move_kwargs)
-        self.assertEqual(self.shape.get_coords(), (move_kwargs['newXCoord'], move_kwargs['newYCoord']))
+        self.assertEqual(
+            self.shape.get_coords(),
+            (move_kwargs["newXCoord"], move_kwargs["newYCoord"]),
+        )
 
     def test_get_drawable(self):
         self.shape.get_drawable()
@@ -31,112 +42,95 @@ class ShapeTests():
 
 
 class CompositeShapeTests(unittest.TestCase, ShapeTests):
-
     def setUp(self):
         self.triangle_init_kwargs = {}
         self.rectangle_init_kwargs = {}
-        self.triangle_init_kwargs['xCoord'] = 50
-        self.triangle_init_kwargs['yCoord'] = 50
-        self.rectangle_init_kwargs['xCoord'] = 50
-        self.rectangle_init_kwargs['yCoord'] = 50
-        triangle_measurements = {
-                'side': 50,
-        }
-        rectangle_measurements = {
-                'width': 50,
-                'length': 50
-        }
-        self.triangle_init_kwargs['measurements'] = triangle_measurements 
-        self.rectangle_init_kwargs['measurements'] = rectangle_measurements
+        self.triangle_init_kwargs["xCoord"] = 50
+        self.triangle_init_kwargs["yCoord"] = 50
+        self.rectangle_init_kwargs["xCoord"] = 50
+        self.rectangle_init_kwargs["yCoord"] = 50
+        triangle_measurements = {"side": 50}
+        rectangle_measurements = {"width": 50, "length": 50}
+        self.triangle_init_kwargs["measurements"] = triangle_measurements
+        self.rectangle_init_kwargs["measurements"] = rectangle_measurements
         self.side = 50
         self.length = 50
         self.width = 50
         self.tag = "test"
-        self.area = (4.33 * triangle_measurements['side'] ** 2) + (rectangle_measurements['width'] * rectangle_measurements['length'])
-        triangle  = Triangle(**self.triangle_init_kwargs)
+        self.area = (4.33 * triangle_measurements["side"] ** 2) + (
+            rectangle_measurements["width"] * rectangle_measurements["length"]
+        )
+        triangle = Triangle(**self.triangle_init_kwargs)
         rectangle = Rectangle(**self.rectangle_init_kwargs)
         self.components = [triangle, rectangle]
         self.shape = CompositeShape(tag=self.tag, components=self.components)
 
 
 class TriangleTests(unittest.TestCase, ShapeTests):
-
     def setUp(self):
         self.init_kwargs = {}
-        self.init_kwargs['xCoord'] = 50
-        self.init_kwargs['yCoord'] = 50
-        measurements = {
-                'side': 50,
-                }
-        self.init_kwargs['measurements'] = measurements
-        self.area = 4.33 * measurements['side'] ** 2
+        self.init_kwargs["xCoord"] = 50
+        self.init_kwargs["yCoord"] = 50
+        measurements = {"side": 50}
+        self.init_kwargs["measurements"] = measurements
+        self.area = 4.33 * measurements["side"] ** 2
         self.shape = Triangle(**self.init_kwargs)
 
     def test_scale(self):
         self.scale_setUp()
-        self.assertEqual(self.before * self.scale_kwargs['factor'] * 2, self.after)
+        self.assertEqual(self.before * self.scale_kwargs["factor"] * 2, self.after)
+
 
 class RectangleTests(unittest.TestCase, ShapeTests):
-
     def setUp(self):
         self.init_kwargs = {}
-        self.init_kwargs['xCoord'] = 50
-        self.init_kwargs['yCoord'] = 50
-        measurements = {
-                'width': 50,
-                'length': 50
-                }
-        self.init_kwargs['measurements'] = measurements 
-        self.area = measurements['width'] * measurements['length']
+        self.init_kwargs["xCoord"] = 50
+        self.init_kwargs["yCoord"] = 50
+        measurements = {"width": 50, "length": 50}
+        self.init_kwargs["measurements"] = measurements
+        self.area = measurements["width"] * measurements["length"]
         self.shape = Rectangle(**self.init_kwargs)
 
     def test_scale(self):
         self.scale_setUp()
-        self.assertEqual(self.before * self.scale_kwargs['factor'] * 2, self.after)
+        self.assertEqual(self.before * self.scale_kwargs["factor"] * 2, self.after)
+
 
 class CircleTests(unittest.TestCase, ShapeTests):
-
     def setUp(self):
         self.init_kwargs = {}
-        self.init_kwargs['xCoord'] = 50
-        self.init_kwargs['yCoord'] = 50
-        measurements = {
-                "radius": 50,
-                }
-        self.init_kwargs['measurements'] = measurements
-        self.area = 3.14 * measurements['radius'] ** 2
+        self.init_kwargs["xCoord"] = 50
+        self.init_kwargs["yCoord"] = 50
+        measurements = {"radius": 50}
+        self.init_kwargs["measurements"] = measurements
+        self.area = 3.14 * measurements["radius"] ** 2
         self.shape = Circle(**self.init_kwargs)
 
     def test_scale(self):
         self.scale_setUp()
-        self.assertEqual(self.before * self.scale_kwargs['factor'] * 2, self.after)
+        self.assertEqual(self.before * self.scale_kwargs["factor"] * 2, self.after)
 
 
 class ImageTests(RectangleTests):
-
     def setUp(self):
         self.init_kwargs = {}
-        self.init_kwargs['xCoord'] = 50
-        self.init_kwargs['yCoord'] = 50
-        measurements = {
-                "width": 50,
-                "length": 50
-                }
-        self.init_kwargs['measurements'] = measurements 
-        self.area = measurements['width'] * measurements['length']
-        self.init_kwargs['filename'] = "test.png"
+        self.init_kwargs["xCoord"] = 50
+        self.init_kwargs["yCoord"] = 50
+        measurements = {"width": 50, "length": 50}
+        self.init_kwargs["measurements"] = measurements
+        self.area = measurements["width"] * measurements["length"]
+        self.init_kwargs["filename"] = "test.png"
         self.shape = Image(**self.init_kwargs)
 
 
 class LineTests(unittest.TestCase, ShapeTests):
-
     def setUp(self):
         self.init_kwargs = {}
-        self.init_kwargs['xCoord'] = 50
-        self.init_kwargs['yCoord'] = 50
-        self.init_kwargs['xCoord2'] = 50
-        self.init_kwargs['yCoord2'] = 50
-        self.init_kwargs['measurements'] = {} 
+        self.init_kwargs["xCoord"] = 50
+        self.init_kwargs["yCoord"] = 50
+        self.init_kwargs["xCoord2"] = 50
+        self.init_kwargs["yCoord2"] = 50
+        self.init_kwargs["measurements"] = {}
         self.area = 0
         self.shape = Line(**self.init_kwargs)
 
@@ -146,12 +140,18 @@ class LineTests(unittest.TestCase, ShapeTests):
 
     def test_move(self):
         move_kwargs = {}
-        move_kwargs['newXCoord'] = 2
-        move_kwargs['newYCoord'] = 2
-        move_kwargs['newXCoord2'] = 4
-        move_kwargs['newYCoord2'] = 4
+        move_kwargs["newXCoord"] = 2
+        move_kwargs["newYCoord"] = 2
+        move_kwargs["newXCoord2"] = 4
+        move_kwargs["newYCoord2"] = 4
         self.shape.move(**move_kwargs)
-        self.assertEqual(self.shape.get_coords(), [(move_kwargs['newXCoord'], move_kwargs['newYCoord']), (move_kwargs['newXCoord2'], move_kwargs['newYCoord2'])])
+        self.assertEqual(
+            self.shape.get_coords(),
+            [
+                (move_kwargs["newXCoord"], move_kwargs["newYCoord"]),
+                (move_kwargs["newXCoord2"], move_kwargs["newYCoord2"]),
+            ],
+        )
 
     def test_scale(self):
         self.scale_setUp()
@@ -160,12 +160,11 @@ class LineTests(unittest.TestCase, ShapeTests):
 
 
 class PointTests(unittest.TestCase, ShapeTests):
-
     def setUp(self):
         self.init_kwargs = {}
-        self.init_kwargs['xCoord'] = 50
-        self.init_kwargs['yCoord'] = 50
-        self.init_kwargs['measurements'] = {} 
+        self.init_kwargs["xCoord"] = 50
+        self.init_kwargs["yCoord"] = 50
+        self.init_kwargs["measurements"] = {}
         self.area = 0
         self.shape = Point(**self.init_kwargs)
 
@@ -177,6 +176,3 @@ class PointTests(unittest.TestCase, ShapeTests):
         self.scale_setUp()
         self.assertEqual(self.before, self.after)
         self.assertEqual(0, self.after)
-
-
-
